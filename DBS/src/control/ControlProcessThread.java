8716afe1424a8a5b1d4308002065152c453e89b5
wso277 <oliveira.wilson93@gmail.com>
2014-03-25 10:28:02 +0000

    SpaceReclaim should be finished

--- DBS/src/control/ControlProcessThread.java
+++ DBS/src/control/ControlProcessThread.java
@@ -5,46 +5,58 @@ import java.util.ArrayList;
 import main.FileManager;
 import main.Main;
 import restore.RestoreProcessThread;
+import main.Chunk;
 
 public class ControlProcessThread extends Thread {
 
-	public static String message;
-	public static ArrayList<String> header;
+    public static String message;
+    public static ArrayList<String> header;
 
-	public ControlProcessThread(String newmessage) {
-		message = newmessage;
-	}
+    public ControlProcessThread(String newmessage) {
+        message = newmessage;
+    }
 
-	public void run() {
+    public void run() {
 
-		header = new ArrayList<String>();
-		
-		String[] tmp = message.split("\\s+");
+        header = new ArrayList<String>();
 
-		for (int i = 0; i < tmp.length; i++) {
-			header.add(tmp[i].trim());
-		}
-		
-		if (header.get(0).equals("GETCHUNK")) {
-			RestoreProcessThread rpt = new RestoreProcessThread(header.get(2),
-					header.get(3));
-		} else if (header.get(0).equals("DELETE")) {
-			deleteProcess();
-			Main.save();
+        String[] tmp = message.split("\\s+");
 
-		} else if (header.get(0).equals("REMOVED")) {
-			
-			System.out.println("removed");
-		} else if (header.get(0).equals("STORED")) {
+        for (int i = 0; i < tmp.length; i++) {
+            header.add(tmp[i].trim());
+        }
 
-		} else {
-			System.err.println("Operation Invalid!");
-		}
+        if (header.get(0).equals("GETCHUNK")) {
+            RestoreProcessThread rpt = new RestoreProcessThread(header.get(2),
+                    header.get(3));
+        } else if (header.get(0).equals("DELETE")) {
+            deleteProcess();
+            Main.save();
 
-	}
+        } else if (header.get(0).equals("REMOVED")) {
 
-	private void deleteProcess() {
-		FileManager del = new FileManager(header.get(1), 0, true);
-		del.delete();
-	}
+            removedProcess();
+        } else if (header.get(0).equals("STORED")) {
+
+        } else {
+            System.err.println("Operation Invalid!");
+        }
+
+    }
+
+    private void removedProcess() {
+        for (int i = 0; i < Main.getDatabase().getChunksSize(); i++) {
+            Chunk chunk = Main.getDatabase().getChunk(i);
+            if (chunk.getFileId().equals(header.get(1)) && (chunk.getChunkNo() == Integer.parseInt(header.get(2)))) {
+                System.out.println("Previous:" + chunk.getChunkNo());
+                chunk.setChunkNo(chunk.getChunkNo() - 1);
+                System.out.println("After:" + chunk.getChunkNo());
+            }
+        }
+    }
+
+    private void deleteProcess() {
+        FileManager del = new FileManager(header.get(1), 0, true);
+        del.delete();
+    }
 }
--- DBS/src/main/Chunk.java
+++ DBS/src/main/Chunk.java
@@ -4,57 +4,57 @@ import java.io.Serializable;
 
 public class Chunk implements Serializable, Comparable<Chunk> {
 
-	private static final long serialVersionUID = 1L;
-	private String fileId;
-	private Integer chunkNo;
-	private Integer repDegree;
-	private int knowReps;
+    private static final long serialVersionUID = 1L;
+    private String fileId;
+    private Integer chunkNo;
+    private Integer repDegree;
+    private int knowReps;
 
-	public Chunk(String file, Integer chunk, Integer rep) {
-		fileId = file;
-		chunkNo = chunk;
-		repDegree = rep;
-	}
+    public Chunk(String file, Integer chunk, Integer rep) {
+        fileId = file;
+        chunkNo = chunk;
+        repDegree = rep;
+    }
 
-	public String getFileId() {
-		return fileId;
-	}
+    public String getFileId() {
+        return fileId;
+    }
 
-	public void setFileId(String newfileId) {
-		fileId = newfileId;
-	}
+    public synchronized void setFileId(String newfileId) {
+        fileId = newfileId;
+    }
 
-	public Integer getChunkNo() {
-		return chunkNo;
-	}
+    public Integer getChunkNo() {
+        return chunkNo;
+    }
 
-	public void setChunkNo(Integer newchunkNo) {
-		chunkNo = newchunkNo;
-	}
+    public synchronized void setChunkNo(Integer newchunkNo) {
+        chunkNo = newchunkNo;
+    }
 
-	public Integer getRepDegree() {
-		return repDegree;
-	}
+    public Integer getRepDegree() {
+        return repDegree;
+    }
 
-	public void setRepDegree(Integer newrepDegree) {
-		repDegree = newrepDegree;
-	}
+    public synchronized void setRepDegree(Integer newrepDegree) {
+        repDegree = newrepDegree;
+    }
 
-	public int getKnowReps() {
-		return knowReps;
-	}
+    public int getKnowReps() {
+        return knowReps;
+    }
 
-	public void setKnowReps(int newknowReps) {
-		knowReps += newknowReps;
-	}
+    public synchronized void setKnowReps(int newknowReps) {
+        knowReps += newknowReps;
+    }
 
-	public Integer getCurrentReps() {
-		return knowReps - repDegree;
-	}
+    public Integer getCurrentReps() {
+        return knowReps - repDegree;
+    }
 
-	@Override
-	public int compareTo(Chunk o) {
-		
-		return getCurrentReps();
-	}
+    @Override
+    public int compareTo(Chunk o) {
+
+        return getCurrentReps();
+    }
 }
