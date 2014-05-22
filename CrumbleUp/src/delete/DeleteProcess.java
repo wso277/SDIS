7b9f9a65af41d9e35f591d76c9449d38d408dbfa
João Cardoso <jmmesquitacardoso@gmail.com>
2014-05-22 16:41:23 +0100

    DeleteProcess class FIX

--- CrumbleUp/src/delete/DeleteProcess.java
+++ CrumbleUp/src/delete/DeleteProcess.java
@@ -15,23 +15,31 @@ public class DeleteProcess extends Thread {
 
     public DeleteProcess() {
         msg = null;
+        running = true;
+        System.out.println("Nasci.");
     }
 
     public void run() {
 
         while (running) {
             Iterator it = Main.getDatabase().getDeletedFiles().entrySet().iterator();
+            System.out.println("Hai :D");
             while (it.hasNext()) {
                 Map.Entry pairs = (Map.Entry) it.next();
+                System.out.println("Correr 1 :D");
+                System.out.println((Integer) pairs.getValue());
                 if ((Integer) pairs.getValue() > 0) {
+                    System.out.println("Correr 2");
                     msg = "DELETE " + (String) pairs.getKey() + Main.getCRLF() + Main.getCRLF();
+                    System.out.println(msg);
                     Main.getControl().send(msg.getBytes(StandardCharsets.ISO_8859_1));
                     it.remove();
                 }
             }
 
             try {
-                sleep(1000 * 60 * 30);
+                System.out.println("Hellooooooooo");
+                sleep(1000 * 30); //TODO change to half an hour
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
