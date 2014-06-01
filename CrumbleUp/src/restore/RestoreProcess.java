af138d60fe2f73a0a4bd278cb259aaded5c59a06
“wso277” <oliveira.wilson93@gmail.com>
2014-06-01 09:52:45 +0100

    Another fix to restore

--- CrumbleUp/src/restore/RestoreProcess.java
+++ CrumbleUp/src/restore/RestoreProcess.java
@@ -36,7 +36,7 @@ class RestoreProcess extends Thread {
             e.printStackTrace();
         }
 
-        Main.getLogger().log("Sent: " + tmp);
+        Main.getLogger().log("RECEIVED: " + tmp);
 
         String[] tmp1 = tmp.split("\\s+");
 
